b1f82b4bc91846fb8d63b915e5c86934d685c1dd
Morten Olsrud <post@dittdesign.com>
2019-06-10 22:35:43 +0200

    Added support for controlling the listening port through environment variables

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 app.use("/",require("./router"));
 app.use("/",express.static(__dirname + "/public"));
 
-app.listen(3000,()=>{
-	console.log("Listening to port 3000. Open http://localhost:3000 to view.");
+if(!process.env.PORT) process.env['PORT'] = 3000;
+
+app.listen(process.env.PORT,()=>{
+	console.log(`Listening to port ${process.env.PORT}. Open http://localhost:${process.env.PORT} to view.`);
 });
